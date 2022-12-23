158746b1d54031e6e025c0b593ba06a1ea09c008
Rishav Raj <iamrishavraj1@gmail.com>
2022-12-23 18:01:08 +0530

    Pasacal Tringle

---
+++ DSA/Arrays/Pasacal_Trinagle.js
@@ -0,0 +1,17 @@
+let numsRows = 5;
+let ans = [];
+console.log(ans);
+for (let i = 0; i < numsRows; i++) {
+  ans[i] = [];
+
+  console.log(`FIRST ${ans}`);
+
+  console.log(`SECOND: ${ans[i][0]}`);
+  ans[i][0] = ans[i][i] = 1;
+
+  for (let j = 1; j < i; j++) {
+    ans[i][j] = ans[i - 1][j] + ans[i - 1][j - 1];
+    // console.log(ans);
+  }
+}
+console.log(ans);
