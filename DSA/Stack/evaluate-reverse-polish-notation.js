40ed75c12269ead68088a744e5ff3eb43236c60f
Rishav Raj <iamrishavraj1@gmail.com>
2023-01-02 21:14:26 +0530

    https://leetcode.com/problems/evaluate-reverse-polish-notation/

---
+++ DSA/Stack/evaluate-reverse-polish-notation.js
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/evaluate-reverse-polish-notation/
+let tokens = [ "2", "1", "+", "3", "*" ];
+let stack = [];
+for (let i = 0; i < tokens.length; i++) {
+  if (tokens[i] === "+") {
+    let a = stack.pop();
+    let b = stack.pop();
+    stack.push(b + a);
+  } else if (tokens[i] === "-") {
+    let a = stack.pop();
+    let b = stack.pop();
+    stack.push(b - a);
+  } else if (tokens[i] === "*") {
+    let a = stack.pop();
+    let b = stack.pop();
+    stack.push(b * a);
+  } else if (tokens[i] === "/") {
+    let a = stack.pop();
+    let b = stack.pop();
+    stack.push(Math.trunc(b / a));
+  } else {
+    stack.push(Number(tokens[i]));
+  }
+}
+console.log(stack.pop());
